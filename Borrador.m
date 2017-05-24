0cb92b515004aa1bd05fbd54e88acab49d4a4b90
Luis Diego Fernandez Coto <luisdifc1993@gmail.com>
2017-05-23 20:44:01 -0600

    cree un archivo para trabajar, no super que nnombre ponerle jajaja

---
+++ Borrador.m
@@ -0,0 +1,19 @@
+%UNIVERSIDAD DE COSTA RICA
+%FACULTAD DE INGENIERÍA 
+%ESCUELA DE INGENIERÍA ELÉCTRICA
+
+%IE0527 - Ingeniería de Comunicaciones
+%I - 2017
+
+
+%Taller de simulación: Transformada de Fourier
+
+%Estudiantes:
+%Luis Diego Fernández Coto
+%Victoria Quirós Cordero
+
+%Profesor:
+%Teodoro Willink Castro
+
+%23/5/17
+
