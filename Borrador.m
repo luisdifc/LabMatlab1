0403b29898d7b245ca73457461c73858b7297157
Luis Diego Fernandez Coto <luisdifc1993@gmail.com>
2017-05-23 21:06:23 -0600

    puse como prueba una graficacion

--- Borrador.m
+++ Borrador.m
@@ -1,19 +1,37 @@
 %UNIVERSIDAD DE COSTA RICA
-%FACULTAD DE INGENIERÍA 
-%ESCUELA DE INGENIERÍA ELÉCTRICA
+%FACULTAD DE INGENIERIA 
+%ESCUELA DE INGENIERIA ELECTRICA
 
-%IE0527 - Ingeniería de Comunicaciones
+%IE0527 - Ingenieria de Comunicaciones
 %I - 2017
 
 
-%Taller de simulación: Transformada de Fourier
+%Taller de simulacion: Transformada de Fourier
 
 %Estudiantes:
-%Luis Diego Fernández Coto
-%Victoria Quirós Cordero
+%Luis Diego Fernandez Coto
+%Victoria Quiros Cordero
 
 %Profesor:
 %Teodoro Willink Castro
 
 %23/5/17
+
+%% MACHOTE GRAFICACION
+t = 0:0.01:(2*pi); 
+x = cos(t);
+y = sin(t);
+plot(t,x);
+hold on; 
+plot(t,y);
+title('Grafica de seno y coseno en 1 periodo');
+xlabel('Eje X');
+ylabel('Eje Y');
+legend('Cos(t)', 'Sin(t)');
+grid on;
+
+%%
+
+
+
 
