1a4e9ce29ff6cca3e3db487baf888a9009dd1181
victoriaqc02 <victoriaqc02@gmail.com>
2017-05-28 21:02:56 -0600

    .m final

--- Borrador.m
+++ Borrador.m
@@ -5,22 +5,21 @@
 %IE0527 - Ingenieria de Comunicaciones
 %I - 2017
 
-
 %Taller de simulacion: Transformada de Fourier
 
 %Estudiantes:
-%Luis Diego Fernandez Coto
-%Victoria Quiros Cordero
+%Luis Diego Fernandez Coto, 
+%Victoria Quirós Cordero, B35527
 
 %Profesor:
 %Teodoro Willink Castro
 
-%23/5/17
+%23/05/17
 
 %% 1. ESTUDIO PRELIMINAR
 clear all;
 
-%Ganancias de Ai
+%Ganancias Ai
 A1 = 0.7;
 A2 = 0.3;
 A3 = 0.4;
@@ -30,23 +29,23 @@ f1 = 440;
 f2 = 3*f1;     
 f3 = 5*f1;  
 
-Fs = 32 * f1;   %frecuencia de muestreo
-N = 512;    %se recomienda que sea una potencia de 2
+Fs = 32 * f1;   % Frecuencia de muestreo
+N = 512;        % Se recomienda que sea una potencia de 2
 
-t = 0:(1/Fs):((N-1)/Fs);    %vector para el tiempo
+t = 0:(1/Fs):((N-1)/Fs);    % Vector para el tiempo
 
 x = (A1*cos(2*pi*f1*t)) + (A2*sin(2*pi*f2*t)) + (A3*cos(2*pi*f3*t));
 
-% Graficacion
-% figure;
-% plot(t*1000,x,'LineWidth',1.8);
-% title('Gráfica de x(t)');
-% xlabel('Tiempo (ms)');
-% ylabel('x(t)');
-% legend('x(t)');
-% set(gcf,'color','w');
-% set(gca,'fontsize', 19);
-% grid on;
+% Grafica
+figure;
+plot(t*1000,x,'LineWidth',1.8);
+title('Gráfica de x(t)');
+xlabel('Tiempo (ms)');
+ylabel('x(t)');
+legend('x(t)');
+set(gcf,'color','w');
+set(gca,'fontsize', 19);
+grid on;
 
 %Potencia de x
 Px = bandpower(x,Fs,[0 Fs/2]);
@@ -68,77 +67,77 @@ T = table(Freqs,Power,'RowNames',Harmonic)
 PxTotal
 
 %% 2. TRANSFORMADA DE FOURIER
-f = -(Fs/2):(Fs/N):((Fs/2)-(Fs/N)); %vector de frecuencia
+f = -(Fs/2):(Fs/N):((Fs/2)-(Fs/N)); % Vector de frecuencia
 
-%Transformada de fourier X(F) de x(t)
-X = 1/N*fftshift(fft(x,N)); %fftshift centra la sennal
+% Transformada de fourier X(F) de x(t)
+X = 1/N*fftshift(fft(x,N)); % fftshift centra x(t)
 
-%Graficacion de la parte real e imaginaria
-% figure;
-% plot (f, real(X),'LineWidth',1.8);
-% hold on;
-% plot (f, imag(X),'LineWidth',1.8);
-% title('Gráfica de X(F)');
-% xlabel('Frecuencia (Hz)');
-% ylabel('X(F)');
-% legend('Parte real', 'Parte imaginaria');
-% set(gcf,'color','w');
-% set(gca,'fontsize', 19);
-% grid on;
+% Grafica de la parte real e imaginaria
+figure;
+plot (f, real(X),'LineWidth',1.8);
+hold on;
+plot (f, imag(X),'LineWidth',1.8);
+title('Gráfica de X(F)');
+xlabel('Frecuencia (Hz)');
+ylabel('X(F)');
+legend('Parte real', 'Parte imaginaria');
+set(gcf,'color','w');
+set(gca,'fontsize', 19);
+grid on;
 
-% Grafiacion del espectro de Fourier de x
-% figure;
-% stem(f, abs(X),'LineWidth',1.8);    %espectro de Fourier
-% title('Espectro de Fourier de X(F)');
-% xlabel('Frecuencia (Hz)');
-% ylabel('Magnitud');
-% legend('Magnitud X(F)');
-% set(gcf,'color','w');
-% set(gca,'fontsize', 19);
-% grid on;
+% Grafica del espectro de Fourier de x
+figure;
+stem(f, abs(X),'LineWidth',1.8);    % Espectro de Fourier
+title('Espectro de Fourier de X(F)');
+xlabel('Frecuencia (Hz)');
+ylabel('Magnitud');
+legend('Magnitud X(F)');
+set(gcf,'color','w');
+set(gca,'fontsize', 19);
+grid on;
 
 %% 3. DENSIDAD ESPECTRAL DE POTENCIA
-f_dB = 0:(Fs/N):((Fs/2)-(Fs/N)); %vector de frecuencia
+f_dB = 0:(Fs/N):((Fs/2)-(Fs/N)); % Vector de frecuencia
 
-%Obtenemos Sxx
+% Obteniendo Sxx
 Sxx = periodogram(x,rectwin(N),N, Fs, 'onesided');
 Sxx = Fs/N*Sxx(1:N/2)';
-SxxdB = 10*log10(Sxx);  %Sxx expresado en decibelios
+SxxdB = 10*log10(Sxx);  % Sxx expresado en dB
 
-%Graficando Sxx en dBW/Hz
-% figure;
-% plot(f_dB,SxxdB,'LineWidth',1.8);
-% title('Gráfica de Sxx');
-% xlabel('F (Hz)');
-% ylaber('Magnitd (dBW)');
-% legend('Sxx(F)');
-% set(gcf,'color','w');
-% set(gca,'fontsize', 19);
-% grid on;
+% Graficando Sxx en dBW/Hz
+figure;
+plot(f_dB,SxxdB,'LineWidth',1.8);
+title('Gráfica de Sxx');
+xlabel('F (Hz)');
+ylabel('Magnitd (dBW)');
+legend('Sxx(F)');
+set(gcf,'color','w');
+set(gca,'fontsize', 19);
+grid on;
 
-%Potencia de Sxx
+% Potencia de Sxx
 PxSxx = 0;
 
 for i = 1:1:length(Sxx)
    PxSxx = Sxx(i)+ PxSxx; 
 end
 
-PxSxx 
+PxSxx % Imprimir resultado
 
 % Sxx a partir de FFT
 Sxx1 = 2*abs(X(N/2+1:N)).^2;
 SxxdB1 = 10*log10(Sxx1);  %Sxx expresado en decibelios
 
 %Graficando Sxx1 en dBW/Hz
-% figure;
-% plot(f_dB,SxxdB1,'LineWidth',1.8);
-% title('Gráfica de Sxx1');
-% xlabel('F (Hz)');
-% ylabel('Magnitd (dBW)');
-% legend('Sxx1(F)');
-% set(gcf,'color','w');
-% set(gca,'fontsize', 19);
-% grid on;
+figure;
+plot(f_dB,SxxdB1,'LineWidth',1.8);
+title('Gráfica de Sxx1');
+xlabel('F (Hz)');
+ylabel('Magnitd (dBW)');
+legend('Sxx1(F)');
+set(gcf,'color','w');
+set(gca,'fontsize', 19);
+grid on;
 
 %Potencia de Sxx
 PxSxx1 = 0;
@@ -147,44 +146,67 @@ for i = 1:1:length(Sxx1)
    PxSxx1 = Sxx(i)+ PxSxx1; 
 end
 
-PxSxx1 
+PxSxx1 % Imprimir resultado
 
 %% 4. FILTRADO
-%filtro FIR pasa-bajos de orden 32 con frecuencia de media potencia de fc = f 2 
+% Filtro FIR pasa-bajos de orden 32 con frecuencia de media potencia de f2
 D = designfilt('lowpassfir','FilterOrder',32,'HalfPowerFrequency',f2,'SampleRate',Fs);
 
-%respuesta en frecuencia del filtro
+% Respuesta en frecuencia del filtro
 % freqz(D, f, Fs);
 
-%Filtrando x con el filtro disennado
+% Filtrando x con el filtro disennado
 y = filtfilt(D,x);
 
-%Transformada de fourier Y(F) de y(t)
+% Transformada de fourier Y(F) de y(t)
 Y = 1/N*fftshift(fft(y,N)); 
 
-%Grafiacion del espectro de Fourier de Y
-% figure;
-% stem(f, abs(Y),'LineWidth',1.8);    %espectro de Fourier
-% title('Especto de Fourier de Y(F)');
-% xlabel('Frecuencia (Hz)');
-% %ylabel('X(F)');
-% legend('Magnitud Y(F)');
-% set(gcf,'color','w');
-% set(gca,'fontsize', 19);
-% grid on;
+% Grafica de y(t)
+figure;
+plot(t*1000,y,'LineWidth',1.8);
+title('Gráfica de y(t)');
+xlabel('Tiempo (ms)');
+ylabel('x(t)');
+legend('x(t)');
+set(gcf,'color','w');
+set(gca,'fontsize', 19);
+grid on;
+
+%Grafica del espectro de Fourier de Y
+figure;
+stem(f, abs(Y),'LineWidth',1.8);    % Espectro de Fourier
+title('Espectro de Fourier de Y(F)');
+xlabel('Frecuencia (Hz)');
+ylabel('Y(F)');
+legend('Magnitud Y(F)');
+set(gcf,'color','w');
+set(gca,'fontsize', 19);
+grid on;
 
 %% RETO: FILTRADO DE UNA SENNAL DE AUDIO
-[w, Fsreto] = audioread('song.wav');
+[w, Fsreto] = audioread('Roxanne.wav');
 
 Nw = length(w);
 
-fw = 0:(Fsreto/Nw):((Fsreto/2)-(Fsreto/Nw)); %vector de frecuencia
+fw = 0:(Fsreto/Nw):((Fsreto/2)-(Fsreto/Nw)); % Vector de frecuencia
 
-%Obtenemos Sxx
+% Obteniendo Sww
 Sww = periodogram(w,rectwin(Nw),Nw, Fsreto, 'onesided');
 Sww = Fsreto/Nw*Sww(1:floor(Nw/2))';
-SwwdB = 10*log10(Sww);  %Sxx expresado en decibelios
+SwwdB = 10*log10(Sww);  % Sww expresado en decibelios
 
+% Grafica de Sww en dBW/Hz
+figure;
+plot(fw,SwwdB,'LineWidth',1.8);
+title('Gráfica de Sww');
+xlabel('F (Hz)');
+ylabel('Magnitd (dBW)');
+legend('Sww(F)');
+set(gcf,'color','w');
+set(gca,'fontsize', 19);
+grid on;
+
+% Filtro IIR pasa-bajos de orden 8 con frecuencia de media potencia de fc = 2000 Hz 
 Dw = designfilt('lowpassiir','FilterOrder',8,'HalfPowerFrequency',2000,'SampleRate',Fsreto);
 
 v = filtfilt(Dw,w);
@@ -193,6 +215,17 @@ Svv = periodogram(v,rectwin(Nw),Nw,Fsreto,'onesided');
 Svv = Fsreto/Nw*Svv(1:floor(Nw/2))';
 SvvdB = 10*log10(Svv);
 
-audiowrite('song_filterd.wav',v,Fsreto);
+% Grafica de Svv en dBW/Hz
+figure;
+plot(fw,SvvdB,'LineWidth',1.8);
+title('Gráfica de Svv');
+xlabel('F (Hz)');
+ylabel('Magnitd (dBW)');
+legend('Svv(F)');
+set(gcf,'color','w');
+set(gca,'fontsize', 19);
+grid on;
+
+audiowrite('Roxanne_filterd.wav',v,Fsreto);
 
 
