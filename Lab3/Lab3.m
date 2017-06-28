fc613a3d9fefdbd727b4d5dc4cc04cea974acf78
victoriaqc02 <victoriaqc02@gmail.com>
2017-06-28 15:46:35 -0600

    Avance

--- Lab3/Lab3.m
+++ Lab3/Lab3.m
@@ -18,43 +18,21 @@
 
 %% 1. ESTUDIO PRELIMINAR
 
-img = imread('Pokemon.jpeg');
-%figure
-%imshow(img);
-
-img_gris = rgb2gray(img);
-%figure
-%imshow(img_gris);
-
-img_gris_bin = dec2bin(img_gris);
-
-img_gris_bin_size = size(img_gris_bin);
-%img_gris_reshape = reshape(img_gris_bin, [1,8*img_gris_bin_size(1)])';
-
-img_gris_reshape = reshape(img_gris_bin, [],1);
+img = imread('Squirtle.png');
+imgBW = rgb2gray(img);
+%imshow(imgBW);
+imgBW_bin = dec2bin(imgBW);
+imgBW_v = reshape(imgBW_bin,[],2);
 
 %% 2. MODULACION 4PAM
 
-pammod_obj = modem.pammod('M', 2, 'InputType', 'bit','SymbolOrder', 'gray');
+imgBW_mod = pammod(bin2dec(imgBW_v), 4);
+
+% Prueba de funcionamiento
+% imgBW_v(125,:)
+% imgBW_v(7,:)
 % 
-double(img_gris_reshape);
-% 
-% % img_gris_reshape_d = double(img_gris_reshape);
-img_sim = modulate(pammod_obj, double(img_gris_reshape));
-
-
-% for j=1:1:length(img_gris_reshape)/2
-% for i=1:2:length(img_gris_reshape)/2
-%     conv = strcat(img_gris_reshape(i), img_gris_reshape(i+1))
-%     str2num(conv)
-%     img_gris_gray(j) = pammod(str2num(conv), 2)
-% end
-% end
-
-%b = bin2gray(bin2dec(img_gris_bin),'pam', 4)
-
-
-
-
+% imgBW_mod(125)
+% imgBW_mod(7)
 
 
