561d9a6739bb5f117c88ece58282572b1f6a8f34
Luis Diego Fernandez Coto <luisdifc1993@gmail.com>
2017-06-28 13:39:01 -0600

    directorio para el lab 3

---
+++ Lab3/Lab3.m
@@ -0,0 +1,22 @@
+%UNIVERSIDAD DE COSTA RICA
+%FACULTAD DE INGENIERIA 
+%ESCUELA DE INGENIERIA ELECTRICA
+
+%IE0527 - Ingenieria de Comunicaciones
+%I - 2017
+
+%Taller de simulación: Modulación digital
+
+%Estudiantes:
+%Luis Diego Fernandez Coto, B22492
+%Victoria Quirós Cordero, B35527
+
+%Profesor:
+%Teodoro Willink Castro
+
+%28/06/17
+
+%% 1. ESTUDIO PRELIMINAR
+
+
+
